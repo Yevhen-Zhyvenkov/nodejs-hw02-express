b67e741213875785dad97b57283f40280d14d3c3
Yevhen-Zhyvenkov <zheka123730@gmail.com>
2023-08-29 21:44:52 +0300

    виправлення

--- models/contacts.js
+++ models/contacts.js
@@ -35,7 +35,7 @@ const addSchema = Joi.object({
 const updateFavoriteSchema = Joi.object({
   favorite: Joi.boolean()
     .required()
-    .messages({"any.required": `missing field favorite field`}),
+    .messages({"any.required": `missing field favorite`}),
 });
 const schemas = {
   addSchema,
